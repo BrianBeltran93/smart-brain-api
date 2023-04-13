6691d448df80930be3fea0896517d3663ea7fa1f
Brian Beltran <brianbeltran93@gmail.com>
2023-04-12 21:21:13 -0400

    db info change

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
-        user : 'postgres',
-        password : 'test',
-        database : 'smart-brain'
+        host : 'dpg-cgrl06fdvk4rjsu3lo10-a.ohio-postgres.render.com',
+        user : 'smartbraindb_cp0b_user',
+        password : 'TXt6QhAdoY24VxmFnhiYYZrVwzkEQVrB',
+        database : 'smartbraindb_cp0b'
     }
 });
 
